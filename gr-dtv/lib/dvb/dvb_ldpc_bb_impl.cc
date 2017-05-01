3a186aadc19ddb61d106ca2a46f8fb71ab3b2b03
Kristian Maier <kristian.maier@gmx.de>
2017-05-01 15:34:04 +0200

    optimized C3_5 code, (skip infobit reloading)

--- gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
+++ gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
@@ -628,49 +628,85 @@ for (int row = 0; row < ROWS; row++) { \
         }
         // First zero all the parity bits
         memset(p, 0, sizeof(unsigned char) * plen);
-//        for (int j = 0; j < (int)nbch; j++) {
-//          out[i + j] = in[consumed];
-//          consumed++;
-//        }
         memcpy(out+i, in+consumed, nbch*sizeof(unsigned char));
         consumed+=nbch;
 
+          switch(code_rate){
+              //really fast optimization here only for C3_5 code for now
+              //TODO: even slightly faster if puncturing test is skipped for this code?
+              case C3_5:
+              {
+                  //const int first_part_len=ldpc_encode.table_length*36/108/12;
+                  constexpr int first_part_len=233280*36/108/12;
 
-        // now do the parity checking
-        //loop unrolling
-        for (int j = 0; j < ldpc_encode.table_length; j++) {
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-            j++;
-            p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
-        }
+                  int j=0;
+                  //avoid reloading the data bits
+                  for (int i = 0; i < first_part_len; i++) {
+                      //1 infobit affects 12 parity bits, + 12 loop unrolling
+                      unsigned char d_enc = d[ldpc_encode.d[j]];
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                  }
+
+                  //const int sec_len=ldpc_encode.table_length*72/108/4/3;
+                  constexpr int sec_len=233280*72/108/4/3;
+
+                  for (int i = 0; i < sec_len; i++) {
+                      //1 infobit affects 3 parity bits + 4*3 loop unrolling
+                      unsigned char d_enc = d[ldpc_encode.d[j]];
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+
+                      d_enc = d[ldpc_encode.d[j]];
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+
+                      d_enc = d[ldpc_encode.d[j]];
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+
+                      d_enc = d[ldpc_encode.d[j]];
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                      p[ldpc_encode.p[j++]] ^= d_enc;
+                  }
+              }
+                  break;
+              default:
+                  //loop unrolling (8) for other cases
+                  for (int j = 0; j < ldpc_encode.table_length; j++) {
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                      j++;
+                      p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
+                  }
+                  break;
+          }
+
 
 
         if (P != 0) {
@@ -693,6 +729,7 @@ for (int row = 0; row < ROWS; row++) { \
 
         const int l = (plen - Xp);
 
+        //loop unrolling (8)
         for (int j = 1; j < l; j++) {
           p[j] ^= p[j-1];
           j++;
@@ -710,7 +747,6 @@ for (int row = 0; row < ROWS; row++) { \
           j++;
           p[j] ^= p[j-1];
         }
-
 
 
         if (signal_constellation == MOD_128APSK) {
