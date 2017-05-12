2ca96d1954bef5c9a9f7d768a9dab59df5b74cf4
Florian <fischbacher-flo@online.de>
2017-05-12 23:50:29 +0200

    General code optimization

--- gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
+++ gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
@@ -614,8 +614,7 @@ for (int row = 0; row < ROWS; row++) { \
       p = &out[nbch];
       int consumed = 0;
       int puncture, index;
-    
-        
+      
       for (int i = 0; i < noutput_items; i += frame_size) {
         if (Xs != 0) {
           s = &shortening_buffer[0];
@@ -629,14 +628,17 @@ for (int row = 0; row < ROWS; row++) { \
         }
         // First zero all the parity bits
         memset(p, 0, sizeof(unsigned char) * plen);
-        for (int j = 0; j < (int)nbch; j++) {
-          out[i + j] = in[consumed];
+        while(consumed < (int)nbch) {
+          out[i + consumed] = boost::move(in[consumed]);
+          //out[i + consumed] = in[consumed];
           consumed++;
-        }
+        } 
+        
         // now do the parity checking
         for (int j = 0; j < ldpc_encode.table_length; j++) {
           p[ldpc_encode.p[j]] ^= d[ldpc_encode.d[j]];
         }
+      
         if (P != 0) {
           puncture = 0;
           for (int j = 0; j < plen; j += P) {
@@ -655,7 +657,7 @@ for (int row = 0; row < ROWS; row++) { \
           p = &out[nbch];
         }
         for (int j = 1; j < (plen - Xp); j++) {
-          p[j] ^= p[j-1];
+          p[j] ^= boost::move(p[j-1]);
         }
         if (signal_constellation == MOD_128APSK) {
           for (int j = 0; j < 6; j++) {
