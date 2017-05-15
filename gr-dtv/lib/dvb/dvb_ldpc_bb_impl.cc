aa33f3874e0fd7cbe4936202a8a6298eff7c5555
Florian <fischbacher-flo@online.de>
2017-05-15 21:13:55 +0200

    Replaced loop with memcpy

--- gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
+++ gr-dtv/lib/dvb/dvb_ldpc_bb_impl.cc
@@ -24,6 +24,7 @@
 
 #include <gnuradio/io_signature.h>
 #include "dvb_ldpc_bb_impl.h"
+#include <emmintrin.h>
 
 namespace gr {
   namespace dtv {
@@ -628,10 +629,10 @@ for (int row = 0; row < ROWS; row++) { \
         }
         // First zero all the parity bits
         memset(p, 0, sizeof(unsigned char) * plen);
-        for (int j = 0; j < (int)nbch; j++) {
-          out[i + j] = in[consumed];
-          consumed++;
-        } 
+        
+        // Instead of copy byte per byte, copy the whole array at once
+        memcpy(out+i, in, nbch);
+        consumed = nbch;
         
         // now do the parity checking
         for (int j = 0; j < ldpc_encode.table_length; j=j+36) {
@@ -692,9 +693,11 @@ for (int row = 0; row < ROWS; row++) { \
           }
           p = &out[nbch];
         }
+        
         for (int j = 1; j < (plen - Xp); j++) {
           p[j] ^= p[j-1];
         }
+        
         if (signal_constellation == MOD_128APSK) {
           for (int j = 0; j < 6; j++) {
             p[j + plen] = 0;
