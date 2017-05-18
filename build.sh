c9912f910434977358848193970e08baba5e9459
root <andreas.trassl@gmx.de>
2017-05-18 21:53:35 +0200

    tried changing prefetching variable

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ cmakeopts+=(
     '-DENABLE_GR_CTRLPORT=OFF'
 )
 
-export CXXFLAGS="-march=skylake -mtune=skylake"
+export CXXFLAGS="-march=skylake -mtune=skylake --param simulataneos-prefetches=4"
 
 cmake ${cmakeopts[@]} $SRC
 
