6ae572573e914c1699fc69568a0e878cc09806a9
Alexander Blömeke <alexander.bloemeke@rwth-aachen.de>
2017-05-20 17:48:50 +0200

    part 2

--- gr-dtv/lib/dvbt2/dvbt2_pilotgenerator_cc_impl.cc
+++ gr-dtv/lib/dvbt2/dvbt2_pilotgenerator_cc_impl.cc
@@ -2700,12 +2700,22 @@ namespace gr {
           if (j < N_P2) {
 
 			  //copying zeros is faster than initializing?
-			  __m256i in_m256i = _mm256_setzero_si256();
+			  __m256 in_m256 = _mm256_setzero_ps();
 			  for ( int loop_i = 0; loop_i < left_nulls / 4; loop_i++) {//1215
-				  _mm256_store_si256((__m256i*)out, in_m256i);
-				  out += 4;
+				  _mm256_store_ps((__m256*)&out, in_m256);
+				  out ++;
+				  out++;
+				  out++;
+				  out++;
 			  }
 
+
+			  //__m256i *in_m256i;
+			  //for (unsigned int loop_i = 0; loop_i< nbch / 32; loop_i++) {//1215
+				 // in_m256i = (__m256i*)&in[consumed];
+				 // _mm256_store_si256((__m256i*)&out[i + 32 * loop_i], *in_m256i);
+				 // consumed += (int)32;
+			  //}
 
 
             //for (int n = 0; n < left_nulls; n++) {
