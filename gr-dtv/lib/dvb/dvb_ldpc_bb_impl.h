d120750cff9e1336bacd2843760d2fe6004dd9f0
Thomas Lambertz <thomas.lambertz1@rwth-aachen.de>
2017-05-21 00:44:11 +0200

    Added Multithreading in LDPC Coder

--- gr-dtv/lib/dvb/dvb_ldpc_bb_impl.h
+++ gr-dtv/lib/dvb/dvb_ldpc_bb_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2015,2016 Free Software Foundation, Inc.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -24,10 +24,15 @@
 #include <gnuradio/dtv/dvb_ldpc_bb.h>
 #include "dvb_defines.h"
 
+typedef struct ldpc_encode_table_item{
+    int d;
+    int p;
+    bool operator< (const ldpc_encode_table_item& right) const { return p < right.p; }
+}ldpc_encode_table_item;
+
 typedef struct{
     int table_length;
-    int d[LDPC_ENCODE_TABLE_LENGTH];
-    int p[LDPC_ENCODE_TABLE_LENGTH];
+    ldpc_encode_table_item item[LDPC_ENCODE_TABLE_LENGTH];
 }ldpc_encode_table;
 
 namespace gr {
@@ -114,13 +119,16 @@ namespace gr {
       const static int ldpc_tab_11_45M[22][11];
       const static int ldpc_tab_1_3M[30][13];
 
+      void thread_loop(unsigned char *p, const unsigned char *d, int start, int end);
+
+
      public:
       dvb_ldpc_bb_impl(dvb_standard_t standard, dvb_framesize_t framesize, dvb_code_rate_t rate, dvb_constellation_t constellation);
       ~dvb_ldpc_bb_impl();
 
       void forecast (int noutput_items, gr_vector_int &ninput_items_required);
 
-      int general_work(int noutput_items,
+      __attribute__((optimize(0))) int general_work(int noutput_items,
                        gr_vector_int &ninput_items,
                        gr_vector_const_void_star &input_items,
                        gr_vector_void_star &output_items);
@@ -130,4 +138,3 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_DTV_DVB_LDPC_BB_IMPL_H */
-
