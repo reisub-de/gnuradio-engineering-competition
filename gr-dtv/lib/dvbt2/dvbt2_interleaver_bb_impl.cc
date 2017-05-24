c23cf67bf8c01fd5d82f8478012811fe2a4d9e18
Kilian Kiekenap <kilian@ramv.de>
2017-05-24 09:57:59 +0200

    Moved c-pointers into array

--- gr-dtv/lib/dvbt2/dvbt2_interleaver_bb_impl.cc
+++ gr-dtv/lib/dvbt2/dvbt2_interleaver_bb_impl.cc
@@ -381,9 +381,10 @@ namespace gr {
               mux = &mux256[0];
             }
             rows = frame_size / (mod * 2);
-			const unsigned char *c1, *c2, *c3, *c4, *c5, *c6, *c7, *c8;
-			const unsigned char *c9, *c10, *c11, *c12, *c13, *c14, *c15, *c16;
-			c1 = &tempv[0];
+			//const unsigned char *c1, *c2, *c3, *c4, *c5, *c6, *c7, *c8;
+			//const unsigned char *c9, *c10, *c11, *c12, *c13, *c14, *c15, *c16;
+			const unsigned char* c[16];
+			/*c1 = &tempv[0];
 			c2 = &tempv[rows];
 			c3 = &tempv[rows * 2];
 			c4 = &tempv[rows * 3];
@@ -398,7 +399,23 @@ namespace gr {
 			c13 = &tempv[rows * 12];
 			c14 = &tempv[rows * 13];
 			c15 = &tempv[rows * 14];
-			c16 = &tempv[rows * 15];
+			c16 = &tempv[rows * 15];*/
+			c[0] = &tempv[0];
+			c[1] = &tempv[rows];
+			c[2] = &tempv[rows * 2];
+			c[3] = &tempv[rows * 3];
+			c[4] = &tempv[rows * 4];
+			c[5] = &tempv[rows * 5];
+			c[6] = &tempv[rows * 6];
+			c[7] = &tempv[rows * 7];
+			c[8] = &tempv[rows * 8];
+			c[9] = &tempv[rows * 9];
+			c[10] = &tempv[rows * 10];
+			c[11] = &tempv[rows * 11];
+			c[12] = &tempv[rows * 12];
+			c[13] = &tempv[rows * 13];
+			c[14] = &tempv[rows * 14];
+			c[15] = &tempv[rows * 15];
 			for (int i = 0; i < noutput_items; i += packed_items) {
               for (int k = 0; k < nbch; k++) {
                 tempu[k] = *in++;
@@ -422,22 +439,22 @@ namespace gr {
               }
               index = 0;
               for (int j = 0; j < rows; j++) {
-                tempu[index++] = c1[j];
-                tempu[index++] = c2[j];
-                tempu[index++] = c3[j];
-                tempu[index++] = c4[j];
-                tempu[index++] = c5[j];
-                tempu[index++] = c6[j];
-                tempu[index++] = c7[j];
-                tempu[index++] = c8[j];
-                tempu[index++] = c9[j];
-                tempu[index++] = c10[j];
-                tempu[index++] = c11[j];
-                tempu[index++] = c12[j];
-                tempu[index++] = c13[j];
-                tempu[index++] = c14[j];
-                tempu[index++] = c15[j];
-                tempu[index++] = c16[j];
+                tempu[index++] = c[0][j];
+                tempu[index++] = c[1][j];
+                tempu[index++] = c[2][j];
+                tempu[index++] = c[3][j];
+                tempu[index++] = c[4][j];
+                tempu[index++] = c[5][j];
+                tempu[index++] = c[6][j];
+                tempu[index++] = c[7][j];
+                tempu[index++] = c[8][j];
+                tempu[index++] = c[9][j];
+                tempu[index++] = c[10][j];
+                tempu[index++] = c[11][j];
+                tempu[index++] = c[12][j];
+                tempu[index++] = c[13][j];
+                tempu[index++] = c[14][j];
+                tempu[index++] = c[15][j];
               }
               index = 0;
               for (int d = 0; d < frame_size / (mod * 2); d++) {
@@ -463,15 +480,24 @@ namespace gr {
               mux = &mux256s[0];
             }
             rows = frame_size / mod;
-			const unsigned char *c1, *c2, *c3, *c4, *c5, *c6, *c7, *c8;
-			c1 = &tempv[0];
+			//const unsigned char *c1, *c2, *c3, *c4, *c5, *c6, *c7, *c8;
+			const unsigned char* c[8];
+			/*c1 = &tempv[0];
             c2 = &tempv[rows];
             c3 = &tempv[rows * 2];
             c4 = &tempv[rows * 3];
             c5 = &tempv[rows * 4];
             c6 = &tempv[rows * 5];
             c7 = &tempv[rows * 6];
-            c8 = &tempv[rows * 7];
+            c8 = &tempv[rows * 7];*/
+			c[0] = &tempv[0];
+            c[1] = &tempv[rows];
+            c[2] = &tempv[rows * 2];
+            c[3] = &tempv[rows * 3];
+            c[4] = &tempv[rows * 4];
+            c[5] = &tempv[rows * 5];
+            c[6] = &tempv[rows * 6];
+            c[7] = &tempv[rows * 7];
             for (int i = 0; i < noutput_items; i += packed_items) {
               for (int k = 0; k < nbch; k++) {
                 tempu[k] = *in++;
@@ -495,14 +521,14 @@ namespace gr {
               }
               index = 0;
               for (int j = 0; j < rows; j++) {
-                tempu[index++] = c1[j];
-                tempu[index++] = c2[j];
-                tempu[index++] = c3[j];
-                tempu[index++] = c4[j];
-                tempu[index++] = c5[j];
-                tempu[index++] = c6[j];
-                tempu[index++] = c7[j];
-                tempu[index++] = c8[j];
+				tempu[index++] = c[0][j];
+                tempu[index++] = c[1][j];
+                tempu[index++] = c[2][j];
+                tempu[index++] = c[3][j];
+                tempu[index++] = c[4][j];
+                tempu[index++] = c[5][j];
+                tempu[index++] = c[6][j];
+                tempu[index++] = c[7][j];
               }
               index = 0;
               for (int d = 0; d < frame_size / mod; d++) {
