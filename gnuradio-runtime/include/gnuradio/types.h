51c8c2f3d93192ff8dd28fd044d1455b4a09115b
Zhiwei Han <hanzw356255531@icloud.com>
2017-05-24 23:39:19 +0200

    add the thread pool to type.h

--- gnuradio-runtime/include/gnuradio/types.h
+++ gnuradio-runtime/include/gnuradio/types.h
@@ -25,6 +25,9 @@
 
 #include <gnuradio/api.h>
 #include <boost/shared_ptr.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
 #include <vector>
 #include <stddef.h>				// size_t
 
@@ -61,5 +64,34 @@ typedef unsigned short 		gr_uint16;
 typedef unsigned int   		gr_uint32;
 typedef unsigned long long	gr_uint64;
 #endif	/* HAVE_STDINT_H */
+
+#ifndef HAVE_THREAD_POOL
+#define HAVE_THREAD_POOL
+// The definition of boost_asio based thread pool
+struct ThreadPool {
+    typedef boost::scoped_ptr<boost::asio::io_service::work> boost_asio_worker;
+
+    ThreadPool(size_t pool_size) :m_service(), m_working(new boost::asio::io_service::work(m_service)) {
+        while(pool_size--)
+        {
+            m_thread_group.create_thread(boost::bind(&boost::asio::io_service::run, &(this->m_service)));
+        }
+    }
+
+    template<class F>
+        void enqueue(F f){m_service.post(f);}
+
+    ~ThreadPool() {
+        m_working.reset(); //allow run() to exit
+        m_thread_group.join_all();
+        m_service.stop();
+    }
+
+    private:
+    boost::asio::io_service m_service; //< the io_service we are wrapping
+    boost_asio_worker m_working;
+    boost::thread_group m_thread_group; //< need to keep track of threads so we can join them
+};
+#endif/* HAVE_THREAD_POOL */
 
 #endif /* INCLUDED_GR_TYPES_H */
