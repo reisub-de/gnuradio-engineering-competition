88ffd3817e1a260eb568d45bd12feffd34519946
ga74zot <Christian.Gajek@tum.de>
2017-05-26 00:08:21 +0200

    Speed up Bit Interleaver- Bigfix1

--- gr-dtv/lib/dvbt2/dvbt2_interleaver_bb_impl.cc
+++ gr-dtv/lib/dvbt2/dvbt2_interleaver_bb_impl.cc
@@ -526,45 +526,105 @@ namespace gr {
       return noutput_items;
     }
 
-    const int Interleaver_Test::twist16n[8] = {0, 0, 2, 4, 4, 5, 7, 7 };
+    const int dvbt2_interleaver_bb_impl::twist16n[8] =
+    {
+      0, 0, 2, 4, 4, 5, 7, 7
+    };
 
-    const int Interleaver_Test::twist64n[12] = {0, 0, 2, 2, 3, 4, 4, 5, 5, 7, 8, 9};
+    const int dvbt2_interleaver_bb_impl::twist64n[12] =
+    {
+      0, 0, 2, 2, 3, 4, 4, 5, 5, 7, 8, 9
+    };
 
-    const int Interleaver_Test::twist256n[16] = {0, 2, 2, 2, 2, 3, 7, 15, 16, 20, 22, 22, 27, 27, 28, 32};
+    const int dvbt2_interleaver_bb_impl::twist256n[16] =
+    {
+      0, 2, 2, 2, 2, 3, 7, 15, 16, 20, 22, 22, 27, 27, 28, 32
+    };
 
-    const int Interleaver_Test::twist16s[8] = {0, 0, 0, 1, 7, 20, 20, 21};
+    const int dvbt2_interleaver_bb_impl::twist16s[8] =
+    {
+      0, 0, 0, 1, 7, 20, 20, 21
+    };
 
-    const int Interleaver_Test::twist64s[12] = {0, 0, 0, 2, 2, 2, 3, 3, 3, 6, 7, 7};
+    const int dvbt2_interleaver_bb_impl::twist64s[12] =
+    {
+      0, 0, 0, 2, 2, 2, 3, 3, 3, 6, 7, 7
+    };
 
-    const int Interleaver_Test::twist256s[8] = {0, 0, 0, 1, 7, 20, 20, 21};
+    const int dvbt2_interleaver_bb_impl::twist256s[8] =
+    {
+      0, 0, 0, 1, 7, 20, 20, 21
+    };
 
-    const int Interleaver_Test::mux16[8] = {7, 1, 4, 2, 5, 3, 6, 0};
+    const int dvbt2_interleaver_bb_impl::mux16[8] =
+    {
+      7, 1, 4, 2, 5, 3, 6, 0
+    };
 
-    const int Interleaver_Test::mux64[12] = {11, 7, 3, 10, 6, 2, 9, 5, 1, 8, 4, 0};
+    const int dvbt2_interleaver_bb_impl::mux64[12] =
+    {
+      11, 7, 3, 10, 6, 2, 9, 5, 1, 8, 4, 0
+    };
 
-    const int Interleaver_Test::mux256[16] = {15, 1, 13, 3, 8, 11, 9, 5, 10, 6, 4, 7, 12, 2, 14, 0};
+    const int dvbt2_interleaver_bb_impl::mux256[16] =
+    {
+      15, 1, 13, 3, 8, 11, 9, 5, 10, 6, 4, 7, 12, 2, 14, 0
+    };
 
-    const int Interleaver_Test::mux16_35[8] = {0, 5, 1, 2, 4, 7, 3, 6};
+    const int dvbt2_interleaver_bb_impl::mux16_35[8] =
+    {
+      0, 5, 1, 2, 4, 7, 3, 6
+    };
 
-    const int Interleaver_Test::mux16_13[8] = {6, 0, 3, 4, 5, 2, 1, 7};
+    const int dvbt2_interleaver_bb_impl::mux16_13[8] =
+    {
+      6, 0, 3, 4, 5, 2, 1, 7
+    };
 
-    const int Interleaver_Test::mux16_25[8] = {7, 5, 4, 0, 3, 1, 2, 6};
+    const int dvbt2_interleaver_bb_impl::mux16_25[8] =
+    {
+      7, 5, 4, 0, 3, 1, 2, 6
+    };
 
-    const int Interleaver_Test::mux64_35[12] = {2, 7, 6, 9, 0, 3, 1, 8, 4, 11, 5, 10};
+    const int dvbt2_interleaver_bb_impl::mux64_35[12] =
+    {
+      2, 7, 6, 9, 0, 3, 1, 8, 4, 11, 5, 10
+    };
 
-    const int Interleaver_Test::mux64_13[12] = { 4, 2, 0, 5, 6, 1, 3, 7, 8, 9, 10, 11};
+    const int dvbt2_interleaver_bb_impl::mux64_13[12] =
+    {
+      4, 2, 0, 5, 6, 1, 3, 7, 8, 9, 10, 11
+    };
 
-    const int Interleaver_Test::mux64_25[12] = {4, 0, 1, 6, 2, 3, 5, 8, 7, 10, 9, 11};
+    const int dvbt2_interleaver_bb_impl::mux64_25[12] =
+    {
+      4, 0, 1, 6, 2, 3, 5, 8, 7, 10, 9, 11
+    };
 
-    const int Interleaver_Test::mux256_35[16] = {2, 11, 3, 4, 0, 9, 1, 8, 10, 13, 7, 14, 6, 15, 5, 12};
+    const int dvbt2_interleaver_bb_impl::mux256_35[16] =
+    {
+      2, 11, 3, 4, 0, 9, 1, 8, 10, 13, 7, 14, 6, 15, 5, 12
+    };
 
-    const int Interleaver_Test::mux256_23[16] = {7, 2, 9, 0, 4, 6, 13, 3, 14, 10, 15, 5, 8, 12, 11, 1};
+    const int dvbt2_interleaver_bb_impl::mux256_23[16] =
+    {
+      7, 2, 9, 0, 4, 6, 13, 3, 14, 10, 15, 5, 8, 12, 11, 1
+    };
 
-    const int Interleaver_Test::mux256s[8] = {7, 3, 1, 5, 2, 6, 4, 0};
+    const int dvbt2_interleaver_bb_impl::mux256s[8] =
+    {
+      7, 3, 1, 5, 2, 6, 4, 0
+    };
 
-    const int Interleaver_Test::mux256s_13[8] = {4, 0, 1, 2, 5, 3, 6, 7};
+    const int dvbt2_interleaver_bb_impl::mux256s_13[8] =
+    {
+      4, 0, 1, 2, 5, 3, 6, 7
+    };
 
-    const int Interleaver_Test::mux256s_25[8] = {4, 0, 5, 1, 2, 3, 6, 7};
+    const int dvbt2_interleaver_bb_impl::mux256s_25[8] =
+    {
+      4, 0, 5, 1, 2, 3, 6, 7
+    };
 
   } /* namespace dtv */
 } /* namespace gr */
