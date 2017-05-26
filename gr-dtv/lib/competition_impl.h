b94df033c73511f159f174f46e4ed072987f97c2
Zhiwei Han <hanzw356255531@icloud.com>
2017-05-27 00:30:36 +0200

    add competition block

---
+++ gr-dtv/lib/competition_impl.h
@@ -0,0 +1,53 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2017 Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_DTV_COMPETITION_IMPL_H
+#define INCLUDED_DTV_COMPETITION_IMPL_H
+
+#include <gnuradio/dtv/competition.h>
+
+namespace gr {
+  namespace dtv {
+
+    class competition_impl : public competition
+    {
+     private:
+      // Nothing to declare in this block.
+
+     public:
+      competition_impl();
+      ~competition_impl();
+
+      // Where all the action really happens
+      void forecast (int noutput_items, gr_vector_int &ninput_items_required);
+
+      int general_work(int noutput_items,
+           gr_vector_int &ninput_items,
+           gr_vector_const_void_star &input_items,
+           gr_vector_void_star &output_items);
+    };
+
+  } // namespace dtv
+} // namespace gr
+
+#endif /* INCLUDED_DTV_COMPETITION_IMPL_H */
+
