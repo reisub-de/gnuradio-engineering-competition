7703f402422220a15e5c9988bc6a3725fd689e37
Zhiwei Han <hanzw356255531@icloud.com>
2017-05-28 21:41:00 +0200

    remove multithread of cell interleaver

--- gr-dtv/lib/dvbt2/dvbt2_cellinterleaver_cc_impl.h
+++ gr-dtv/lib/dvbt2/dvbt2_cellinterleaver_cc_impl.h
@@ -24,42 +24,6 @@
 #include <gnuradio/dtv/dvbt2_cellinterleaver_cc.h>
 #include "dvb/dvb_defines.h"
 
-struct FuncHandlerDataStruct {
-  FuncHandlerDataStruct(  
-          const gr_complex *arg_in,
-          gr_complex *arg_out,
-          const int arg_val_numSmallTIBlocks,
-          const int arg_val_numBigTIBlocks,
-          const int arg_val_FECBlocksPerSmallTIBlock,
-          const int arg_val_FECBlocksPerBigTIBlock,
-          const int arg_val_fecblocks,
-          const int arg_val_cell_size,
-          const int arg_val_pn_degree,
-          const int *arg_val_permutations
-  ) :
-          in(arg_in),
-          out(arg_out),
-          val_numSmallTIBlocks(arg_val_numSmallTIBlocks),
-          val_numBigTIBlocks(arg_val_numBigTIBlocks),
-          val_FECBlocksPerSmallTIBlock(arg_val_FECBlocksPerSmallTIBlock),
-          val_FECBlocksPerBigTIBlock(arg_val_FECBlocksPerBigTIBlock),
-          val_fecblocks(arg_val_fecblocks),
-          val_cell_size(arg_val_cell_size),
-          val_pn_degree(arg_val_pn_degree),
-          val_permutations(arg_val_permutations) {}
-
-  const gr_complex *in;
-  gr_complex *out;
-  const int val_numSmallTIBlocks;
-  const int val_numBigTIBlocks;
-  const int val_FECBlocksPerSmallTIBlock;
-  const int val_FECBlocksPerBigTIBlock;
-  const int val_fecblocks;
-  const int val_cell_size;
-  const int val_pn_degree;
-  const int *val_permutations;
-};
-
 namespace gr {
   namespace dtv {
 
@@ -82,8 +46,6 @@ namespace gr {
      public:
       dvbt2_cellinterleaver_cc_impl(dvb_framesize_t framesize, dvb_constellation_t constellation, int fecblocks, int tiblocks);
       ~dvbt2_cellinterleaver_cc_impl();
-
-      static void func_handler(FuncHandlerDataStruct);
 
       int work(int noutput_items,
                gr_vector_const_void_star &input_items,
